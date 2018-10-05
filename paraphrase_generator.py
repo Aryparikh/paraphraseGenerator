4e5729358a084c8da0bcc12dd4f9466eb9fd5e0f
Aryparikh <Aryparikh@users.noreply.github.com>
2018-10-05 14:56:04 +0530

    Update paraphrase_generator.py

--- paraphrase_generator.py
+++ paraphrase_generator.py
@@ -26,12 +26,17 @@ import mtranslate
 from googletrans import Translator
 import re
 
-languages = ["ar", "sq", "be", "bg" "af", "ca", "zh-CN", "zh-TW", "hr",
-             "cs", "da", "nl", "et", "en", "tl", "fi", "fr", "gl", "de",
-             "el", "iw", "hi", "hu", "is", "id", "ga", "it", "ja", "ko",
-             "lv", "lt", "mk", "ms", "mt", "no", "fa", "pl", "pt", "ro",
-             "ru", "sr", "sk", "sl", "es", "sw", "sv", "th", "tr", "uk",
-             "vi", "cy", "yi"]
+languages = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs',
+             'bg', 'ca', 'ceb', 'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da',
+             'nl', 'en', 'eo', 'et', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 
+             'el', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is', 
+             'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 
+             'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 
+             'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ny', 'ps', 
+             'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 
+             'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 
+             'tl', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 
+             'cy', 'xh', 'yi', 'yo', 'zu']
 
 
 
